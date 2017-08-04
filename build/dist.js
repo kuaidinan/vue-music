0001b9266f2f09f3c4c8682e07e2edb19accfccc
kuaidinan <351789535@qq.com>
2017-08-04 14:58:20 +0800

    添加script

--- build/dist.js
+++ build/dist.js
@@ -1,10 +1,14 @@
 var config = require('../config/index.js');
 var express = require('express'),
     app = express(),
-    path = require('path');
+    path = require('path')
+    opn = require('opn');
+
+var uri = 'http://localhost:' + config.dist.port;
 
 app.use(express.static(path.join(__dirname,'../dist')))
 
 app.listen(config.dist.port,() => {
   console.log('my server is start,port is %s',config.dist.port);
+  opn(uri);
 })
